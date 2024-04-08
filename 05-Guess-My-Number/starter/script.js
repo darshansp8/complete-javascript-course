8464b5728dd6137d9c3035bb525cf5582e294b16
Darshan Sakpal <d.sakpal38@gmail.com>
2024-04-08 11:23:18 +0100

    Implemented basic DOM manipulation to check input value

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = "Correct Number";
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23; 
+
+document.querySelector('.check').addEventListener('click', () => {
+    const guess = Number(document.querySelector('.guess').value);
+    if (!guess) {
+        document.querySelector('.message').textContent = "⛔️ No Number!";
+    }
+});
