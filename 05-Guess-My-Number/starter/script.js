aee6d7b2cf1bf7a8cd90ec359ccfc336c1bde480
Darshan Sakpal <d.sakpal38@gmail.com>
2024-04-08 11:50:16 +0100

    Implemented the game logic to comapre the user guess and secret number

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -7,9 +7,39 @@
 
 // document.querySelector('.guess').value = 23; 
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+// Comparing the user number with the secret number
 document.querySelector('.check').addEventListener('click', () => {
     const guess = Number(document.querySelector('.guess').value);
     if (!guess) {
         document.querySelector('.message').textContent = "⛔️ No Number!";
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = "🎉 Correct Number!";
+        document.querySelector('body').style.backgroundColor = "#60b347";
+        document.querySelector('.number').style.width = "30rem";
+    }
+    else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = "Too High!";
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = "🤯 You lost the game!";
+            document.querySelector('.score').textContent = 0;
+        }
+
+    }
+    else if (guess < secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = "Too Low!";
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = "🤯 You lost the game!";
+            document.querySelector('.score').textContent = 0;
+        }
     }
 });
